2fef1e3280715f3e65129e180ca7490c2938261c
FoxGeoff <fox.geoffrey@gmail.com>
2018-12-20 11:27:24 -0800

    Add demo path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'contactmanager', loadChildren: './contactmanager/contactmanager.module#ContactmanagerModule'},
-  // { path: 'demo', loadChildren: './demo/demo.module#DemoModule'}, 
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule'}, 
   { path: '**', redirectTo: 'contactmanager'},
 ]
 
